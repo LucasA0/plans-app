53e20ef7227d000ce971314a1206f6beed16d108
Lucas Araujo <lucascdearaujo444@gmail.com>
2023-04-27 11:23:00 -0300

    inicio do desenvolvimento da responsividade de clientes

--- src/pages/Planos/index.jsx
+++ src/pages/Planos/index.jsx
@@ -4,6 +4,7 @@ import { checkboxGroup } from "../../utils/Menus/menuItems."
 import { useContext, useState } from "react";
 import { PlansContext } from "../../contexts/Plans/PlansContext";
 import Loading from "../../components/Loading";
+import { RiMenu3Fill } from "react-icons/ri";
 
 function Planos() {
   const [hide, setHide] = useState(false);
@@ -24,26 +25,31 @@ function Planos() {
           <Typography>
             Planos ativos: {allPlans?.filter(plans => !plans.archived).length}
           </Typography>
-          <Stack sx={{ width: '60%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly' }}>
+          <Stack sx={{ width: {xs: '0px', sm: '60%', md: '60%'}, height: '100%', flexDirection: 'row',
+            alignItems: 'center', justifyContent: 'space-evenly', display: {xs: 'none', sm: 'flex', md: 'flex'} }}>
             {checkboxGroup.map((check) => (
               <label
-                style={{ display: 'flex', alignItems: 'center', height: '100%', justifyContent: 'space-evenly',
-                  cursor: 'pointer', width: '16%'
+                style={{ display: 'flex', alignItems: 'center', width: '14%', height: '100%', justifyContent: 'space-evenly',
+                  cursor: 'pointer'
               }}
                 key={check.id}
               >
                 <input
                   type="checkbox" name={check.name} id={check.name} value={check.value}
-                  style={{accentColor: '#D40066'}}
                 />
                 {check.name}
               </label>
             ))}
-            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', width: {sm: '20%', md: '15%'}}}>
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', width: '20%'}}>
               <img src="./assets/icons/Filter.png" alt="filtro" />
               <Typography variant="h7" fontWeight="bold" >Filtrar</Typography>
             </Box>
           </Stack>
+          <Box sx={{width: {xs: '25%', sm: '0px', md: '0px'}, height: {xs: '100%', sm: '100%', md: '0px'},
+            display: {xs: 'flex', sm: 'none', md: 'none'}, alignItems: 'center', justifyContent: 'flex-end'}}
+            >
+            <RiMenu3Fill style={{width: '50px', height: '35px', cursor: 'pointer'}} onClick={() => {}}/>
+          </Box>
         </Stack>
         {loading === true ? <Loading /> : <Box sx={{width: '100%', height: '500px', overflowY: 'auto'}}>
           <CompletePlansCard />
